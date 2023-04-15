90e8d167d55754810d9f1dd1b05ef30c60fb34d8
TamNhiDuong <yanxinyee@gmail.com>
2023-04-15 15:21:24 +0300

    Exc 4.4 test the total likes sum

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const initialValue = 0
+    const totalLikes = blogs.reduce((accumulator, item) => accumulator + item.likes, initialValue)
+    return totalLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
